c1359d4fd85828d8f0f2604321e9490b7686c5f9
Persephone <kurtuson92@gmail.com>
2026-09-05 01:35:40 +0000

    2nd Prob solved

--- Python-Basics.py
+++ Python-Basics.py
@@ -2,4 +2,38 @@ my_name = "Kurt Angel D. Uson"
 my_age = 22
 favourite_subject = "arts"
 
-print(f"My Name is, {my_name},Im {my_age}, years old, and i love {favourite_subject}")
+print(f"My Name is, {my_name},Im {my_age}, years old, and i love {favourite_subject}")\
+
+a = 10
+b = 10.5
+c = "10"
+d = True
+
+#int
+#float
+#string
+#boolean
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+#2.b
+
+a = 10
+c = "5"
+num = int(c)
+d = 3.9
+num2 = int(d)
+
+result = a + num
+
+print(result)
+print(num2)
+
+# BONUS: convert 3.9 into an int. What happens to the decimal part?
+# thge decimal was not included in whole number
+
+
